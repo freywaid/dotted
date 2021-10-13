f8973b7b365ef239adbe4a382f219c13a21f258e
frey <frey@futurefamily.com>
2021-10-13 00:18:58 -0700

    rev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rt") as f:
 
 setuptools.setup(
     name="dotted-notation",
-    version="0.6.3",
+    version="0.6.4",
     author="Frey Waid",
     author_email="logophage1@gmail.com",
     description="Dotted notation parser with pattern matching",
