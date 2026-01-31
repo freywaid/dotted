7d22c3c2e2b40f2bbf1d3817ac65caec25ab3eae
frey <frey@modfin.com>
2026-01-31 11:24:17 -0800

    bump version to 0.18.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rt") as f:
 
 setuptools.setup(
     name="dotted_notation",
-    version="0.18.1",
+    version="0.18.2",
     author="Frey Waid",
     author_email="logophage1@gmail.com",
     description="Dotted notation for safe nested data traversal with optional chaining, pattern matching, and transforms",
